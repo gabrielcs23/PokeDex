7b72274c5a0323d9782cdba328bd20e98f30a058
gabrielcs23 <gabrielcs23@hotmail.com>
2020-03-08 22:02:37 -0300

    Define UI básica com Bootstrap e altera logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 
+import { Navbar, Nav } from 'react-bootstrap';
+
 function App() {
-  return (
-    <div className="App">
-      <h1>PokéDex</h1>
-    </div>
-  );
+    return (
+        <Fragment>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Navbar.Brand href="/">PokéDex</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+                    <Nav>
+                        <Nav.Link href="/">
+                            <i className="fas fa-home"></i>
+                        </Nav.Link>
+                        <Nav.Link href="#sobre">Sobre</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <div className="container mt-5">
+                TODO
+            </div>
+        </Fragment>
+    );
 }
 
 export default App;
