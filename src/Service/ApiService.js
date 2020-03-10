6c9d2cde8b0c0e7496815487c3e6b66f28d781e0
gabrielcs23 <gabrielcs23@hotmail.com>
2020-03-09 23:40:11 -0300

    Recuperando espécie: - Provê mais detalhes do Pokemon

--- src/Service/ApiService.js
+++ src/Service/ApiService.js
@@ -11,6 +11,15 @@ const ApiService = {
         const res = await axios.get(url);
         const { height, weight, id, name, sprites, stats, types} = res.data;
         return { altura: height/10, peso: weight/10, id: id, name: name, spriteUrl: sprites.front_default, stats: stats, types: types }
+    },
+
+    getPokemonSpecies: async (id) => {
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+        const { capture_rate, color, flavor_text_entries, evolution_chain } = res.data;
+        const descricao = flavor_text_entries.find(desc => {
+            return desc.language.name === 'en' && desc.version.name === "x";
+        });
+        return { captureRate: Math.round((capture_rate / 255) * 100), color: color.name, descricao: descricao.flavor_text, urlEvolucao: evolution_chain.url};
     }
 
 }
